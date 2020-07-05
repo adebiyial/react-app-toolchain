135fdddf2298a520faa377b62c7b85b7b61c2032
adebiyi adedotun <adebiyi@kobo360.com>
2020-07-05 12:29:19 +0100

    Include format command

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return <h1>Hello World!</h1>;
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
