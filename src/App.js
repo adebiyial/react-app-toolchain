a3f006d1fd9557d5164aa5edc62df760f676f27d
adebiyi adedotun <adebiyi@kobo360.com>
2020-07-05 12:41:49 +0100

    Include styling with css, scss, and styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 // src/App.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app.css';
+import './app.scss';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  text-transform: uppercase;
+`;
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <StyledApp>
+      <h1 className='clr-red bg-clr-black'>Hello World!</h1>
+    </StyledApp>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
