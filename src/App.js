19ac569d606b74cf84e3b13ede1122a73caa82f9
adebiyi adedotun <adebiyi@kobo360.com>
2020-07-05 12:23:05 +0100

    Running the app with parcel

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// src/App.js
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
